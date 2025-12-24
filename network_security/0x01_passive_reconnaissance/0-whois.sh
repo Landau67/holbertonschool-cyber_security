8f8f3fb8f9b456df2bd4e36d0f976ed0a5f1c439
landau <landau@landau.landau>
2025-12-24 14:42:51 +0400

    ...

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F: '/^(Registrant|Admin|Tech)/ { gsub(/\r/, ""); key=$1; val=substr($0, index($0, ":")+1); gsub(/^ */, "", val); if (key ~ /Street/) val = val " "; if (key ~ /Ext/) key = key ":"; print key "," val }'
+whois "$1" | awk -F: '/^(Registrant|Admin|Tech)/ { gsub(/\r/, ""); key=$1; val=substr($0, index($0, ":")+1); gsub(/^ */, "", val); if (key ~ /Street/) val = val " "; if (key ~ /Ext/) key = key ":"; print key ", " val }'
