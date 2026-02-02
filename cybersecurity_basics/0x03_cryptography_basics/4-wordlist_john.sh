e2de162391b94da32f7c4bd40215dadf4f334f12
landau <landau@landau.landau>
2026-02-02 15:50:21 +0400

    ...

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt $1 --format=Raw-MD5 && john --show --format=Raw-MD5 $1 | cut -d ":" -f 2 > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt $1 --format=Raw-MD5 && john --show --format=Raw-MD5 $1 | cut -d: -f2 | head -n -2 > 4-password.txt
