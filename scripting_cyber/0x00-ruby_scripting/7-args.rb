8c3b2c05e74195746177b39fca6b82da3f4a19da
Landau67 <newlandau652@gmail.com>
2026-05-20 14:04:47 +0400

    args

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -1,8 +1,8 @@
-#!/usr/bin/env ruby
 def print_arguments
   if ARGV.empty?
     puts "No arguments provided."
   else
+    puts "Arguments:"
     ARGV.each_with_index { |arg, i| puts "#{i + 1}. #{arg}" }
   end
 end
